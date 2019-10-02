00992b2e5b89e4c3730be243f067f473bf1f7e59
Janos Molnar <janosmolnar1001@gmail.com>
2019-10-02 14:52:25 +0200

    rename

--- zuora-aqua-client-cli.rb
+++ zuora-aqua-client-cli.rb
@@ -1,11 +1,11 @@
 class Spoon < Formula
   desc "Run ZOQL queries through AQuA from the CLI"
-  homepage "https://github.com/endreymarcell/zuora-aqua-cli"
+  homepage "https://github.com/endreymarcell/zuora-aqua-client-cli"
   url "todo add this"
   sha256 "todo add this"
 
   def install
-    bin.install "zac"
+    bin.install "zacc"
     lib.install Dir["lib/*"]
     mv "#{prefix}/lib", "#{bin}"
   end
